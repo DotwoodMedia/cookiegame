2a45140e32a8536addc8841e3d5d3da236b00934
Dotwood Media <info@dotwood.media>
2021-04-28 10:36:09 +0200

    Update

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# cookiegame
+# 🍪 Cookie game
+In deze game is gebruik gemaakt van alleen maar Javascript. Alle koekjes worden opgeslagen in een browser cookie die 30 dagen geldig is.
+Dit is vooral bedoeld om Javascript te leren als je net begint. Het is niet te uitgebreid, maar het werkt naar behoren
+
+## Talen die zijn gebruikt
+- Javascript
+- CSS
+- HTML
+
+## Bugs en problemen
+Wees vrij om de <a href="https://github.com/DotwoodMedia/cookiegame/issues">problemen</a> pagina te bekijken. 
+Heb je een probleem, maar die staat er niet bij? Maak dan gerust een nieuw probleem aan.
+
+# 👤 Auteur
+Pascal van Stiphout
+- Website <a href="https://dotwood.media/">https://dotwood.media/</a>
+- Github <a href="https://github.com/DotwoodMedia">@DotwoodMedia</a>
+
+# 📑 Licentie
+© 2021 - Dotwood Media
+Op dit project zit een <a href="https://github.com/DotwoodMedia/cookiegame/blob/main/LICENSE">Apache</a> licentie
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ let cookies = 0;
 let autoClicker = 0;
 let cookieAmount = 1;
 
+// Set a new cookie
 function setCookie(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
@@ -9,6 +10,7 @@ function setCookie(cname, cvalue, exdays) {
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
+// Get a cookie from your browser
 function getCookie(cname) {
     var name = cname + "=";
     var decodedCookie = decodeURIComponent(document.cookie);
@@ -25,6 +27,7 @@ function getCookie(cname) {
     return "";
 }
 
+// Check the cookie
 function checkCookie() {
     var userCookies = getCookie("cookies");
     if (userCookies != "") {
@@ -36,11 +39,125 @@ function checkCookie() {
             setCookie("cookies", userCookies, 30);
         }
     }
+
+    var userClicks = getCookie("cookieAmount");
+    if (userClicks != "") {
+        cookieAmount = parseInt(userClicks);
+        document.getElementById("cookieSecond").innerHTML = cookieAmount;
+    } else {
+        userClicks = cookieAmount;
+        if (userClicks != "" && userClicks != null) {
+            setCookie("cookieAmount", userClicks, 30);
+        }
+    }
+
+    var autoClicks = getCookie("autoCookies");
+    if (autoClicks != "") {
+        autoClicker = parseInt(autoClicks);
+    } else {
+        autoClicks = autoClicker;
+        if (autoClicks != "" && autoClicks != null) {
+            setCookie("autoCookies", autoClicks, 30);
+        }
+    }
 }
 
-function cookieClick() {
-    cookies += cookieAmount;
-
+// Handlers
+function cookieAdd(amount) {
+    cookies += amount;
     setCookie("cookies", cookies, 30);
     document.getElementById("cookieAmount").innerHTML = cookies;
+}
+
+// Voeg die cookies toe
+function cookieClick() {
+    cookieAdd(cookieAmount);
+}
+
+// Per 1 seconde een aantal cookies toevoegen
+function autoCookie() {
+    setInterval(() => {
+        cookieAdd(autoClicker);
+    }, 1000)
+}
+
+// Verdubbel de tellers
+function cookieDubbel() {
+    if (cookies > 20) {
+        // Remove the cookies
+        cookies = cookies - 20;
+        document.getElementById("cookieAmount").innerHTML = cookies;
+        setCookie("cookies", cookies, 30);
+
+        // Set the auto clicker
+        autoClicker = autoClicker * 2;
+        setCookie("autoCookies", autoClicker, 30);
+
+        // Set cookie per click
+        cookieAmount = cookieAmount * 2;
+        document.getElementById("cookieSecond").innerHTML = cookieAmount;
+        setCookie("cookieAmount", cookieAmount, 30);
+    }
+    else {
+        alert("Je hebt geen 20 cookies!")
+    }
+}
+
+// Stel de auto clicker in
+function setAutoClicker() {
+    if (cookies > 50) {
+        // Remove the cookies
+        cookies = cookies - 50;
+        document.getElementById("cookieAmount").innerHTML = cookies;
+        setCookie("cookies", cookies, 30);
+
+        // Set the auto clicker
+        autoClicker += 1;
+        setCookie("autoCookies", autoClicker, 30);
+    }
+    else {
+        alert("Je hebt geen 50 cookies!")
+    }
+}
+
+// Bakkers boost (Alles x5)
+function bakkersBoost() {
+    if (cookies > 200) {
+        // Remove the cookies
+        cookies = cookies - 200;
+        document.getElementById("cookieAmount").innerHTML = cookies;
+        setCookie("cookies", cookies, 30);
+
+        // Set the boost
+        autoClicker = autoClicker * 5;
+        setCookie("autoCookies", autoClicker, 30);
+
+        cookieAmount = cookieAmount * 5;
+        setCookie("cookieAmount", cookieAmount, 30);
+
+        setTimeout(() => {
+            autoClicker = autoClicker / 5;
+            setCookie("autoCookies", autoClicker, 30);
+
+            cookieAmount = cookieAmount / 5;
+            setCookie("cookieAmount", cookieAmount, 30);
+        }, 10000)
+    }
+    else {
+        alert("Je hebt geen 200 cookies!")
+    }
+}
+
+// Reset function
+function resetCookies() {
+    cookies = 0;
+    document.getElementById("cookieAmount").innerHTML = cookies;
+    setCookie("cookies", cookies, 30);
+
+    autoClicker = 0;
+    setCookie("autoCookies", autoClicker, 30);
+
+    cookieAmount = 1;
+    document.getElementById("cookieSecond").innerHTML = cookieAmount;
+    setCookie("cookieAmount", cookieAmount, 30);
 }
